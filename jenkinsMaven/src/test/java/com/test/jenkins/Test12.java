073ef418e061a6ab60f5bbba618bfb456a077a8d
shiva252010 <srishasrinika@gmail.com>
2020-02-24 11:47:33 +0530

    My second commit

--- jenkinsMaven/src/test/java/com/test/jenkins/Test12.java
+++ jenkinsMaven/src/test/java/com/test/jenkins/Test12.java
@@ -4,11 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
-public class Test1 {
+public class Test12 {
 	@Test
 	public void testJenkins()
 	{
-		System.out.println("this is my test one ");
+		
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://www.amazon.com");
+		String title=driver.getTitle();
+		System.out.println(title);
 		
 	}
 

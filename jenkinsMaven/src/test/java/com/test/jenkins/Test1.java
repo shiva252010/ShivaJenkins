1b3bf3a4cecbe34a736ba171d7825e6bed40bd27
shiva252010 <srishasrinika@gmail.com>
2020-02-24 11:02:59 +0530

    My first jenkins repo commit

---
+++ jenkinsMaven/src/test/java/com/test/jenkins/Test1.java
@@ -0,0 +1,12 @@
+package com.test.jenkins;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void testJenkins()
+	{
+		System.out.println("this is my test one ");
+	}
+
+}
